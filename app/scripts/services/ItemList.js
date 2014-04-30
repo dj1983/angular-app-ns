9c529964c994223a20e3c443248c25a37ff9b265
zhongsp <patrick.holynova@gmail.com>
2014-04-30 16:55:46 +0800

    Edit

--- app/scripts/services/ItemList.js
+++ app/scripts/services/ItemList.js
@@ -5,44 +5,35 @@
 angular.module('ToyotaTCheck.services.ItemList', [])
   .provider('ItemList', function() {
 
-      var baseUrl = './';
+    var baseUrl = './';
 
-      this.setBaseUrl = function setBaseUrl(url) {
-        baseUrl = url;
-      };
+    this.setBaseUrl = function setBaseUrl(url) {
+      baseUrl = url;
+    };
 
-      this.$get = ['$http', '$log', function($http, $log) {
+    this.$get = ['$http', '$log',
+      function($http, $log) {
 
         return {
           query: function() {
             var checklists = [];
 
             $http.get(baseUrl + 'checklist.json')
-              // .success(function(data, status) {
-              //   angular.forEach(data, function(item) {
-              //     checklists.push(item);
-              //   });
-              // })
-              // .error(function() {
-              //   $log.log('ERROR');
-              // });
-              .then(function(data) {
-                $log.log(111111);
-                $log.log(data);
-                // data = data.data;
-                // angular.forEach(data, function(item) {
-                //   checklists.push(item);
-                // });
-                //return $http.get(baseUrl + 'temp.json');
-                return this;
-              }).then(function(data) {
-                $log.log(222222);
-                $log.log(data);
-              });
+              .then(
+                function(response) {
+                  angular.forEach(response.data, function(item) {
+                    checklists.push(item);
+                  });
+                },
+                function(error) {
+                  $log.log(error);
+                }
+              );
 
             return checklists;
           }
         };
 
-      }];
-    });
+      }
+    ];
+  });
