39a452dbd5e840ae54ac853a65ab454d7a67a16a
zhongsp <patrick.holynova@gmail.com>
2014-05-09 09:56:27 +0800

    Add custom directive

---
+++ app/scripts/directives/loadingIndicator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** Directive */
+
+angular.module('ToyotaTCheck.directives.loadingIndicator', [])
+  .directive('loadingIndicator', function() {
+
+    return {
+      restrict: 'EA',
+      template: '<div ng-class="{ loading: 1, show: loading.isShow }">Loading...</div>',
+      replace: 1,
+      scope: {},
+      link: function(scope, iElement, iAttrs) {
+
+      }
+    };
+  });
