c0d8f0afbc07a418ba923f619df1d213d1657e4d
zhongsp <patrick.holynova@gmail.com>
2014-05-16 11:20:24 +0800

    Add test

--- app/scripts/directives/scope.js
+++ app/scripts/directives/scope.js
@@ -3,21 +3,31 @@
 
 /** Directive */
 
-/**
- * Test 'scope' prop
- */
-
 angular.module('ToyotaTCheck.directives.scope', [])
-  .directive('childScope', function($rootScope) {
+  /** Text binding with @ */
+  .directive('childScopeOne', function() {
     return {
       scope: {
         text: '@myAttr'
       },
-      template: '<div>From custom directive: <em>{{text}}</em></div>',
+      template: 
+        '<div>From custom directive: <em>{{text}}</em><br />' +
+        'From custom directive: <input type="text" ng-model="text">' +
+        '</div>'
+    };
+  })
+  /** One-way binding with & */
+  .directive('childScopeTwo', function() {
+    return {
+      scope: {
+        inheritedValue: '&myAttr'
+      },
+      template: 
+        '<div>From custom directive: <em>{{data}}</em><br />' +
+        'From custom directive: <input type="text" ng-model="data">' +
+        '</div>',
       link: function(scope, element, attrs) {
-        // scope.theScope = 'Child scope';
-        // console.log(scope === $rootScope);
-        // console.log(scope.foo);
+        scope.data = scope.inheritedValue();
       }
     };
   });
