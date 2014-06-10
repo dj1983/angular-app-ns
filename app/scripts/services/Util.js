9cd62ddc0878a25efc0e3dd2e4b5500745f9d36d
Shengping Zhong <patrick.zhongsp@gmail.com>
2014-06-10 20:58:46 +0800

    fix issue #4

--- app/scripts/services/Util.js
+++ app/scripts/services/Util.js
@@ -4,20 +4,22 @@
 
 angular.module('ToyotaTCheck.services.Util', [])
   .factory('Util', [
+    '$firebase',
     '$log',
-    function($log) {
+    'FirebaseService',
+    function($firebase, $log, FirebaseService) {
 
       return {
         /**
          * @param ids {Array}  Example: ["1", "2", "3"]
          * @param dictionary {Object}  Example: {"1": { "id": "1", "title": "lorem", "children": ["5", "6", "7"] }}
-         * @returns {Array}  
+         * @returns {Array}
          */
         getItemTree: function getItemTree(ids, dictionary) {
           var itemTree = [], item;
 
           angular.forEach(ids, function(id) {
-            item = dictionary[id];
+            item = $firebase(FirebaseService.root.child('/items/' + id));
             if (item.children && item.children.length > 0) {
               item.children = getItemTree(item.children, dictionary);
             }
